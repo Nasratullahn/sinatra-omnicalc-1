eae487c33d59da0e57af10a7c9fcc0a349613146
Nasratullah Nasrat <60459439+Nasratullahn@users.noreply.github.com>
2024-03-18 21:50:54 +0000

    random updated

--- app.rb
+++ app.rb
@@ -35,13 +35,12 @@ get ("/random/new") do
   erb(:random) 
 end
 
-get("/random") do
-  @min = params[:min].to_f
-  @max = params[:max].to_f
-  @random_number = rand(min..max).to_f
- 
- 
-  erb(:randomres)
+get("/random/results") do
+  @min = params.fetch("user_min").to_f
+  @max = params.fetch("user_max").to_f
+  @random_number = rand(@min..@max).to_f
+  
+  erb(:random_result)
 end
 
 get("/") do
